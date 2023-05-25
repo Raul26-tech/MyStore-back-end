4b19ccf6853a8a64e947f78320c32a327874f27f
Raul26_tech <rauletec2020@gmail.com>
2023-05-25 20:43:33 -0300

    Criando repositório e listando usuários

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.listen(port);
 
-app.use("/create-user", createUserRoutes);
+app.use("/users", createUserRoutes);
 
 console.log(
   `\n\n\n| WELCOME TO E-COMMERCE, SERVER IS RUNNING ON PORT ${port} |\n\n\n`
